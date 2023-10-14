305c230313db2adc8a31f6e6c4a4044872e76acb
Gea-Suan Lin <gslin@gslin.org>
2023-10-15 05:14:02 +0800

    Support ai18.pics and idol69.net.

--- src/sites/image/pig69.com.js
+++ src/sites/image/pig69.com.js
@@ -1,6 +1,6 @@
 _.register({
   rule: {
-    host: /^(javball|pig69)\.com$/,
+    host: /^(ai18\.pics|idol69\.net|javball\.com|pig69\.com)$/,
   },
   async ready () {
     const img = $('.fileviewer-file img');
